7171ea6ab1d7dd8608269cbd4edfa415f3eb8fbe
LIMJIYEON59 <chayi2110@gmail.com>
2023-09-08 12:07:08 +0900

    fruitshop_test1 연습

---
+++ react/fruitshop_test1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element=""/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
